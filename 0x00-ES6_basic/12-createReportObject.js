4afde67eccd2cccf6f2c0857b93b4420abcb3104
montybasquiart <owamathias@gmail.com>
2024-12-21 13:50:56 +0000

    Fixed code output error

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,10 @@
 export default function createReportObject(employeesList) {
     return {
-	allEmployees: {
-	    ...employeesList,
-	},
-	getNumberOfDepartments() {
-	    return Object.keys(this.allEmployees).length;
-	},
+        allEmployees: {
+            ...employeesList,
+        },
+        getNumberOfDepartments() {
+            return Object.keys(this.allEmployees).length;
+        },
     };
 }
