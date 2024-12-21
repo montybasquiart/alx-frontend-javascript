b85a54a7116a321825473f5348a8e731f58e0a1a
montybasquiart <owamathias@gmail.com>
2024-12-21 12:42:33 +0000

    Fixed code output error

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,4 @@
 export default function createEmployeesObject(departmentName, employees) {
-    return {
-	[departmentName]: employees
-    };
+    const employeesData = { [departmentName]: employees };
+    return employeesData;
 }
