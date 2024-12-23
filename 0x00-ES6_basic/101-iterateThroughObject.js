008718d23908bdc957f095300e9e20863526f7fc
montybasquiart <owamathias@gmail.com>
2024-12-23 00:07:53 +0000

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {  
+    const employeeName = [];  
+    for (const employee of reportWithIterator) {  
+        employeeName.push(employee);  
+    }  
+    return names.join(' | ');  
+}
