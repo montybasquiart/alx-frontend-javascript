9921636f926ae131d764ed57b3d55e104fb32e95
montybasquiart <owamathias@gmail.com>
2024-12-23 00:38:06 +0000

    Fixed code output error

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-export default function iterateThroughObject(reportWithIterator) {  
-    const names = [];  
-    for (const employee of reportWithIterator) {  
-        names.push(employee);  
-    }  
-    return names.join(' | ');  
-}
-=======
 export default function iterateThroughObject(reportWithIterator) {
-    const returnString = [];
-    for (const report of reportWithIterator) {
-      returnString.push(report);
-    }
-    return returnString.join(' | ');
+  let names = '';
+  for (const item of reportWithIterator) {
+    const prefix = (names.length > 0) ? ' | ' : '';
+    names += `${prefix}${item}`;
   }
->>>>>>> cf6eb95e5e61db81bd82434d0aa3e1e78e05d7db
+  return names;
+}
