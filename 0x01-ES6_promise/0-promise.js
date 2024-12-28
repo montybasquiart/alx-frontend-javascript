6a6c01a5270546c9546530fab87f017a97a83676
montybasquiart <owamathias@gmail.com>
2024-12-28 23:51:36 +0000

    Fixed code output error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,12 @@
-function getResponseFromAPI() => {
-    return new Promise((resolve, reject)) => {
-	setTimeout(() => {
-	    const success = true;
-	    if (success) {
-		resolve("");
-	    } else {
-		reject("");
-	    }
-	}, 1000);
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = true;
+            if (success) {
+                resolve("");
+            } else {
+                reject("");
+            }
+        }, 1000);
     });
 }
