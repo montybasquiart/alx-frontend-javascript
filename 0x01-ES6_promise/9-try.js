65f0717ad2f1bc64175cd9b417649cee27ac183e
Mathias Owa Martins <owamathias@gmail.com>
2024-12-29 19:32:33 +0100

    Throw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (err) {
+    queue.push(`Error: ${err.message}`);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
