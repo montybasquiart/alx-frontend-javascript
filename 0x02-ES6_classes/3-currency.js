1ef40addfe48e8bfa6ed370b352af2ef98049035
Mathias Owa Martins <owamathias@gmail.com>
2025-01-01 19:47:05 +0100

    Fixed problem with the method implementation

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,10 +1,34 @@
 export default class Currency {
   constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+
     this._code = code;
     this._name = name;
   }
 
-  displayFullCurrency() {
-    return (`${this._code} ${this._name}`);
+  get code() {
+    return this._code;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+displayFullCurrency() {
+  return `${this.name} (${this.code})`;
+}
 }
